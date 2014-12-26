cc0bdbc30279eaaf8b20c7abd82b9bed59ad1828
Arky <hitmanarky@gmail.com>
2014-12-26 14:18:02 +0700

    Updated Button code and refactored workflow

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-var data = require("sdk/self").data;
-var mypanel = require("sdk/panel").Panel({
-width: 600,
-height: 500,
-contentURL: "http://webchat.freenode.net/"
-});
-var mywidget = require("sdk/widget").Widget({
-id: "Freenode_Icon",
-label: "Freenode IRC Client",
-width: 16,
-height: 16,
-contentURL: data.url("irc.png"),
-panel: mypanel
+var panels = require("sdk/panel");
+var { ToggleButton } = require("sdk/ui/button/toggle");
+
+// Panel code
+var panel = panels.Panel({
+    width: 600,
+    height: 500,
+    contentURL: "http://webchat.freenode.net/",
+    onHide: handleHide
 });
 
+
+// Button code
+var button = ToggleButton({
+    id: "Freenode_Icon",
+    label: "Freenode IRC Client",
+    icon: {
+	"16": "./irc.png"
+    },
+    onChange: handleChange
+});
+
+// Handle Button Change
+function handleChange(state) {
+    if (state.checked){
+	panel.show({position:button});
+    }
+
+}
+
+// Handle panel hiding
+function handleHide(){
+    button.state("window", {checked: false});
+}
